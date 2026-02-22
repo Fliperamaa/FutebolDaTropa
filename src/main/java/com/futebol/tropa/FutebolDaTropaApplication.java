243075806982279f316c9e634c94b84786149320
Fliperamaa <felipechandler59@gmail.com>
2026-02-22 18:03:29 -0300

    0

---
+++ src/main/java/com/futebol/tropa/FutebolDaTropaApplication.java
@@ -0,0 +1,13 @@
+package com.futebol.tropa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutebolDaTropaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FutebolDaTropaApplication.class, args);
+	}
+
+}
